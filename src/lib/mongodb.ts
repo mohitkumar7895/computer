902ef84ab052d8e386746d019d154d4ba3450632
mohitkumar7895 <mohitporwal596@gmail.com>
2026-04-15 17:43:57 +0530

    mongo

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -22,7 +22,7 @@ export async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+    cached.promise = mongoose.connect(MONGODB_URI as string, { bufferCommands: false });
   }
 
   cached.conn = await cached.promise;
